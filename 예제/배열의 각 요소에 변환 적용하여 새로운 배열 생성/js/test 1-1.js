fd9e7359fa4b408f6b24a9378be6d40aecdf4ac8
yeoungsu <130114045+yeoungsu@users.noreply.github.com>
2024-07-31 17:13:56 +0900

    javascript 30일 챌린지 예제 문제 풀이
    아직 map() 메서드를 쓰지 않고 배열의 각 요소에 변환을 적용하여 새로운 배열을 생성하는 것에 대해 이해가 덜 되어
    추가 보충 예정
    days3

---
+++ 예제/배열의 각 요소에 변환 적용하여 새로운 배열 생성/js/test 1-1.js
@@ -0,0 +1,105 @@
+// 예제 배열의 각 요소에 변환 적용하여 새로운 배열 생성
+
+// 문제  :
+// 코드
+// 사용 예시
+// 위 코드의 잘못된 점
+// 수정한 코드
+// 사용 예시
+// 코드 설명
+// 실생활 예시
+
+// 문제 1 : 배열 요소 필터링 filterArray
+// 정수 배열 arr와 필터링 함수 fn이 주어졌을 때, 조건에 맞는 요소들만 포함하는 새로운 배열을 반환하세요.
+// 조건 함수 fn은 배열의 각 요소와 그 인덱스를 인자로 받아 불리언 값을 반환합니다.
+
+// 코드
+let filterArray = function(arr, fn) {
+    const result = []; // 빈 배열
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
+// 사용 예시
+// const arr1 = [1,2,3,4,5];
+// const fn1 = function isEven(n) { return n % 2 === 0;};
+// console.log(filterArray(arr1, fn1)); // [2,4]
+
+// 코드 설명
+
+
+// 실생활 예시
+
+
+// 문제 2 : reduceArray
+// 정수 배열 arr과 누적 함수 fn, 초기값 init이 주어졌을 때, 배열의 모든 요소를 하나의 값으로 누적한 결과를 반환하세요.
+// 누적 함수 fn은 누적값과 현재 값을 인자로 받아 새로운 누적값을 반환합니다.
+
+// 코드
+let reduceArray = function(arr, fn, init) {
+    let accumulator = init;
+    for (let i = 0; i < arr.length; i++) {
+        accumulator = fn(accumulator, arr[i]);
+    }
+    return accumulator;
+}
+
+// 사용 예시
+// const arr1 = [1,2,3,4,5];
+// const fn1 = function sum(acc, n) { return acc + n; };
+// console.log(reduceArray(arr1, fn1, 0)); // 15
+
+// 위 코드의 잘못된 점
+
+
+// 수정한 코드
+
+
+// 사용 예시
+
+
+// 코드 설명
+
+
+// 실생활 예시
+
+
+// 문제 3 : forEachElement
+// 정수 배열 arr과 함수 fn이 주어졌을 때, 배열의 각 요소에 대해 주어진 함수를 실행하세요.
+// 함수 fn 은 배열의 각 요소와 그 인덱스를 인자로 받습니다.
+
+// 코드
+let forEachElement = function(arr, fn) {
+    for (let i = 0; i < arr.length; i++) {
+        fn(arr[i], i);
+    }
+}
+
+// 사용 예시
+const arr1 = [1,2,3,4,5];
+const fn1 = function printElement(n,i) {console.log(`Index ${i} : ${n}`);};
+forEachElement(arr1, fn1);
+// index 0 : 1
+// index 1 : 2
+// index 2 : 3
+// index 3 : 4
+// index 4 : 5
+
+// 위 코드의 잘못된 점
+
+
+// 수정한 코드
+
+
+// 사용 예시
+
+
+// 코드 설명
+
+
+// 실생활 예시
+
